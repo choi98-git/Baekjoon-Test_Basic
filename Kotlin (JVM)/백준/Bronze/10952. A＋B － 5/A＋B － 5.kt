9e3584002f963d7eccdfcc251f5e3a471e930779
choi98-git <79885792+choi98-git@users.noreply.github.com>
2024-03-18 14:51:49 +0900

    [Bronze V] Title: A+B - 5, Time: 124 ms, Memory: 14608 KB -BaekjoonHub

---
+++ Kotlin (JVM)/백준/Bronze/10952. A＋B － 5/A＋B － 5.kt
@@ -0,0 +1,9 @@
+fun main(){
+    while (true){
+        val input = readLine()?.split(" ")?: break
+        val a = input[0].toInt()
+        val b = input[1].toInt()
+        if (a == 0 || b == 0) break
+        println(a+b)
+    }
+}
