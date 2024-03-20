bd44d4a3bfef931c8146fbe74be5090c6179beac
choi98-git <79885792+choi98-git@users.noreply.github.com>
2024-03-20 13:54:10 +0900

    [Bronze V] Title: A+B - 7, Time: 132 ms, Memory: 14528 KB -BaekjoonHub

---
+++ Kotlin (JVM)/백준/Bronze/11021. A＋B － 7/A＋B － 7.kt
@@ -0,0 +1,10 @@
+fun main(){
+    val testCase = readLine()!!.toInt()
+    
+    for (i in 1..testCase){
+        val input = readLine()!!.split(" ")
+        val a = input[0].toInt()
+        val b = input[1].toInt()
+        println("Case #${i}: ${a+b}")
+    }
+}
